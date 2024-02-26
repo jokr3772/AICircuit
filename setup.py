3f744e4faff412628be51c3bc4e61fb8539b10d6
Asal <asal.mehradfar@yahoo.com>
2024-02-26 14:15:40 -0800

    setup update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 def create_folder(path, folder_name):
     if os.path.exists(path):
-        print("{} FOLDER EXIST ALREADY".format(folder_name))
+        print("{} FOLDER EXISTS ALREADY".format(folder_name))
     else:
         os.mkdir(path)
         print("{} FOLDER DOES NOT EXIST, CREATING NOW".format(folder_name))
@@ -17,28 +17,26 @@ def setup():
 
     train_result_folder = os.path.join(pwd, "result_out")
     train_plot_folder = os.path.join(pwd, "out_plot")
-    tmp_netlist_out_folder = os.path.join(pwd, "tmp_out")
     test_data_out_folder = os.path.join(pwd, "data")
 
-    nmos_data_out_path = os.path.join(test_data_out_folder, "nmos")
-    cascode_data_out_path = os.path.join(test_data_out_folder, "cascode")
+    nmos_data_out_path = os.path.join(test_data_out_folder, "SingleStageAmplifier")
+    cascode_data_out_path = os.path.join(test_data_out_folder, "Cascode")
     LNA_data_out_path = os.path.join(test_data_out_folder, "LNA")
-    mixer_data_out_path = os.path.join(test_data_out_folder, "mixer")
-    two_stage_data_out_path = os.path.join(test_data_out_folder, "two_stage")
+    mixer_data_out_path = os.path.join(test_data_out_folder, "Mixer")
+    two_stage_data_out_path = os.path.join(test_data_out_folder, "TwoStage")
     VCO_data_out_path = os.path.join(test_data_out_folder, "VCO")
-    pa_data_out_path = os.path.join(test_data_out_folder, "pa")
+    pa_data_out_path = os.path.join(test_data_out_folder, "PA")
 
-    create_folder(train_result_folder, "TRAIN RESULT")
-    create_folder(train_plot_folder, "TRAIN PLOT")
-    create_folder(tmp_netlist_out_folder, "TEMP NETLIST OUT")
-    create_folder(test_data_out_folder, "TEST DATA OUT")
-    create_folder(nmos_data_out_path, "NMOS DATA OUT")
-    create_folder(cascode_data_out_path, "CASCODE DATA OUT")
-    create_folder(LNA_data_out_path, "LNA DATA OUT")
-    create_folder(mixer_data_out_path, "MIXER DATA OUT")
-    create_folder(two_stage_data_out_path, "TWO STAGE DATA OUT")
-    create_folder(VCO_data_out_path, "VCO DATA OUT")
-    create_folder(pa_data_out_path, "PA DATA OUT")
+    create_folder(train_result_folder, "RESULTS")
+    create_folder(train_plot_folder, "PLOTS")
+    create_folder(test_data_out_folder, "DATA")
+    create_folder(nmos_data_out_path, "SINGLE STAGE AMPLIFIER DATA")
+    create_folder(cascode_data_out_path, "CASCODE DATA")
+    create_folder(LNA_data_out_path, "LNA DATA")
+    create_folder(mixer_data_out_path, "MIXER DATA")
+    create_folder(two_stage_data_out_path, "TWO STAGE DATA")
+    create_folder(VCO_data_out_path, "VCO DATA")
+    create_folder(pa_data_out_path, "PA DATA")
 
 if __name__ == '__main__':
     setup()
