075d7f3f33e61f11f3e974ac0a686b01e41ab0ec
Asal <asal.mehradfar@yahoo.com>
2024-05-09 13:29:01 -0700

    reconstruction

--- reconstruction.py
+++ reconstruction.py
@@ -1,5 +1,6 @@
-from utils import load_visual_config, load_train_config, update_train_config_given_model_type
+from utils import load_visual_config, load_train_config
 from visualutils import *
+
 
 def reload_training_raw_data(result_path):
 
@@ -45,8 +46,6 @@ def reconstruct_comparing_plot(result_paths, new_save_path, train_config = None)
         raise ValueError
 
     epochs = 100 if "epochs" not in train_config else train_config["epochs"]
-    check_every = 20 if "check_every" not in train_config else train_config["check_every"]
-    first_eval = 1 if "first_eval" not in train_config else train_config["first_eval"]
 
     if train_config["compare_dataset"] or train_config["compare_method"]:
         if "multi_train_loss" in result_dict_list[0].keys():
@@ -57,32 +56,6 @@ def reconstruct_comparing_plot(result_paths, new_save_path, train_config = None)
             plot_multiple_loss_with_confidence_comparison(multi_loss, multi_loss_upper_bound, multi_loss_lower_bound,
                                                           labels, train_config["subset"], new_save_path_folder, visual_config, epochs)
 
-        if "multi_test_accuracy" in result_dict_list[0].keys():
-            multi_accuracy = [i["multi_test_accuracy"] for i in result_dict_list]
-            multi_accuracy_upper_bound = [i["multi_test_accuracy_upper_bound"] for i in result_dict_list]
-            multi_accuracy_lower_bound = [[i["multi_test_accuracy_lower_bound"] for i in result_dict_list]]
-            plot_multiple_accuracy_per_epochs_with_confidence_comparison(multi_accuracy, multi_accuracy_upper_bound, multi_accuracy_lower_bound,
-                                                                         labels, train_config["subset"], new_save_path_folder,
-                                                                         visual_config, epochs, check_every, first_eval)
-
-        if "test_margins" in result_dict_list[0].keys():
-            multi_margin = []
-            multi_margin_upper_bound = []
-            multi_margin_lower_bound = []
-
-            for result_dict in result_dict_list:
-                margin_mean, margin_upper_bound, margin_lower_bound = generate_margin_eval_accuracy_given_config(
-                    result_dict["test_margins"],
-                    train_config,
-                    visual_config)
-                multi_margin.append(margin_mean)
-                multi_margin_upper_bound.append(margin_upper_bound)
-                multi_margin_lower_bound.append(margin_lower_bound)
-
-            plot_multiple_margin_with_confidence_comparison(multi_margin, multi_margin_upper_bound,
-                                                            multi_margin_lower_bound, labels, train_config["subset"], new_save_path_folder, visual_config)
-
-
 
 def reconstruct_plot(result_path, new_save_path, train_config = None):
     '''
@@ -90,7 +63,6 @@ def reconstruct_plot(result_path, new_save_path, train_config = None):
     New save path is the new folder name where you want to store the new reconstructed plot
     If the data you want to reconstruct is run before this code change, you have to supply your custom train_config, otherwise leave it as None
     '''
-
 
     visual_config = load_visual_config()
 
@@ -105,17 +77,3 @@ def reconstruct_plot(result_path, new_save_path, train_config = None):
     if "multi_train_loss" in result_dict.keys():
         plot_loss(result_dict["multi_train_loss"], result_dict["multi_train_loss_upper_bound"],
                   result_dict["multi_train_loss_lower_bound"], visual_config, train_config, save_name=os.path.join(new_save_path_folder, "loss.png"))
-
-    if "multi_test_accuracy" in result_dict.keys():
-        plot_accuracy(result_dict["multi_test_accuracy"], result_dict["multi_test_accuracy_upper_bound"],
-                      result_dict["multi_test_accuracy_lower_bound"], visual_config, train_config, save_name=os.path.join(new_save_path_folder, "accuracy.png"))
-
-    if "test_margins" in result_dict.keys():
-        multi_margin_mean, multi_margin_upper_bound, multi_margin_lower_bound = generate_margin_eval_accuracy_given_config(result_dict["test_margins"],
-                                                                                                      train_config,
-                                                                                                      visual_config)
-        plot_margin(multi_margin_mean, multi_margin_upper_bound,
-                    multi_margin_lower_bound, visual_config, train_config, save_name=os.path.join(new_save_path_folder, "margin.png"))
-
-
-
