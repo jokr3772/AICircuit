5b00df4222a1472f4a2f2ca231abe88097be7963
Asal <asal.mehradfar@yahoo.com>
2024-05-09 20:25:48 -0700

    rewriting setup

--- setup.py
+++ setup.py
@@ -14,29 +14,30 @@ def setup():
 
     pwd = os.getcwd()
 
+    result_folder = os.path.join(pwd, "result_out")
+    create_folder(result_folder, "RESULTS")
 
-    train_result_folder = os.path.join(pwd, "result_out")
-    train_plot_folder = os.path.join(pwd, "out_plot")
-    test_data_out_folder = os.path.join(pwd, "data")
+    plot_folder = os.path.join(pwd, "out_plot")
+    create_folder(plot_folder, "PLOTS")
 
-    nmos_data_out_path = os.path.join(test_data_out_folder, "SingleStageAmplifier")
-    cascode_data_out_path = os.path.join(test_data_out_folder, "Cascode")
-    LNA_data_out_path = os.path.join(test_data_out_folder, "LNA")
-    mixer_data_out_path = os.path.join(test_data_out_folder, "Mixer")
-    two_stage_data_out_path = os.path.join(test_data_out_folder, "TwoStage")
-    VCO_data_out_path = os.path.join(test_data_out_folder, "VCO")
-    pa_data_out_path = os.path.join(test_data_out_folder, "PA")
+    data_folder = os.path.join(pwd, "data")
+    create_folder(data_folder, "DATA")
 
-    create_folder(train_result_folder, "RESULTS")
-    create_folder(train_plot_folder, "PLOTS")
-    create_folder(test_data_out_folder, "DATA")
-    create_folder(nmos_data_out_path, "SINGLE STAGE AMPLIFIER DATA")
-    create_folder(cascode_data_out_path, "CASCODE DATA")
-    create_folder(LNA_data_out_path, "LNA DATA")
-    create_folder(mixer_data_out_path, "MIXER DATA")
-    create_folder(two_stage_data_out_path, "TWO STAGE DATA")
-    create_folder(VCO_data_out_path, "VCO DATA")
-    create_folder(pa_data_out_path, "PA DATA")
+    print("\n")
+
+    circuits = ["SingleStageAmplifier", "Cascode", "LNA", "Mixer", "TwoStage", "VCO", "PA", "VCO_PA", "Receiver"] 
+    models = ["MultiLayerPerceptron", "RandomForest", "SupportVectorRegressor", "Transformer"]
+
+    for circuit in circuits:
+        p = os.path.join(data_folder, circuit)
+        create_folder(p, circuit.upper() + " DATA")
+
+        for model in models:
+            pm = os.path.join(data_folder, circuit, model)
+            create_folder(pm, circuit.upper() + " DATA " + model.upper() + " MODEL")
+        
+        print("\n")
+    
 
 if __name__ == '__main__':
     setup()
