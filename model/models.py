aa9bdd6db334d9176adb71659354d1a87c8ae972
Asal <asal.mehradfar@yahoo.com>
2024-05-17 13:28:31 -0700

    model files name change

--- model/models.py
+++ model/models.py
@@ -1,164 +1,107 @@
-import numpy as np
-from Dataset.dataset import BaseDataset
+from sklearn.ensemble import RandomForestRegressor
+   
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.svm import SVR
 
-from utils.utils import generate_metrics_given_config, merge_metrics
-from Model.model_wrapper import SklearnModelWrapper, PytorchModelWrapper, LookupWrapper
+from sklearn.neighbors import KNeighborsRegressor
 
-import os
+import torch.nn as nn
 
-
-def subset_split(X,y,train_percentage, independent = False):
-
-    split_size = np.gcd(int(train_percentage * 100), 100)
-    split_time = int(100 / split_size)
-    # split_time = 1
-    X_size = X.shape[1]
-    combine = np.hstack((X, y))
-
-    if independent:
-        for _ in range(split_time):
-            np.random.shuffle(combine)
-            split_array = np.array_split(combine, split_time)
-            concat_list = [split_array[k] for k in range(len(split_array)) if k != 0]
-            if np.gcd(int(train_percentage * 100), 100) + int(train_percentage * 100) == 100:
-                train_data = np.concatenate(concat_list)
-                validate_data = split_array[0]
-            else:
-                train_data = split_array[0]
-                validate_data = np.concatenate(concat_list)
-            yield train_data[:,:X_size], validate_data[:,:X_size], train_data[:,X_size:], validate_data[:,X_size:]
-            break
-
-    else:
-        np.random.shuffle(combine)
-        split_array = np.array_split(combine, split_time)
-        for st in range(len(split_array)):
-            concat_list = [split_array[k] for k in range(len(split_array)) if k != st]
-            if np.gcd(int(train_percentage * 100), 100) + int(train_percentage * 100) == 100:
-                train_data = np.concatenate(concat_list)
-                validate_data = split_array[st]
-            else:
-                train_data = split_array[st]
-                validate_data = np.concatenate(concat_list)
-            yield train_data[:,:X_size], validate_data[:,:X_size], train_data[:,X_size:], validate_data[:,X_size:]
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
 
 
-class EvalModel:
-    def __init__(self, train_config, model, train_parameter, train_performance, test_parameter, test_performance, simulator, scaler):
-        self.train_config = train_config
-        self.model = model
-        self.train_parameter = train_parameter
-        self.train_performance = train_performance
-        self.test_parameter = test_parameter
-        self.test_performance = test_performance
-        self.simulator = simulator
-        self.scaler = scaler
+class RandomForest():
+    def __init__(self):
+        self.model = RandomForestRegressor()
 
-    def eval(self):
-        
-        train_result = self.model.fit(self.train_performance, self.train_parameter, self.test_performance, self.test_parameter)
+    def fit(self,x,y):
+        self.model.fit(x,y)
+        return self.model
+    
+    def predict(self,x):
+        #Never called
+        return self.model.predict(x)
+    
 
-        test_parameter_prediction = self.model.predict(self.test_performance)
-        train_parameter_prediction = self.model.predict(self.train_performance)
-        inverse_test_parameter, inverse_test_performance = BaseDataset.inverse_transform(test_parameter_prediction, self.test_performance, self.scaler)
-        inverse_train_parameter, inverse_train_performance = BaseDataset.inverse_transform(train_parameter_prediction, self.train_performance, self.scaler)
+class SupportVector():
+    def __init__(self):
+        self.model = MultiOutputRegressor(SVR(kernel="rbf"))
 
-        # print(self.train_config)
-        np.save(os.path.join(self.simulator.arguments["out"], self.train_config["model_name"], "test_x.npy"), inverse_test_parameter)
-        np.save(os.path.join(self.simulator.arguments["out"], self.train_config["model_name"], "test_y.npy"), inverse_test_performance)
-        np.save(os.path.join(self.simulator.arguments["out"], self.train_config["model_name"], "train_x.npy"), inverse_train_parameter)
-        np.save(os.path.join(self.simulator.arguments["out"], self.train_config["model_name"], "train_y.npy"), inverse_train_performance)
-        
-        self.model.reset()
-        return train_result
+    def fit(self,x,y):
+        self.model.fit(x,y)
+        return self.model
+    
+    def predict(self,x):
+        #Never called
+        return self.model.predict(x)
 
 
-class ModelEvaluator:
-    def __init__(self, parameter, performance, eval_dataset, metric, simulator, train_config, model):
+class KNeighbors():
+    def __init__(self):
+        self.model = KNeighborsRegressor()
 
-        if np.any(performance == 0):
-            raise ValueError("There is 0 in performance before scaling")
+    def fit(self,x,y):
+        self.model.fit(x,y)
+        return self.model
+    
+    def predict(self,x):
+        #Never called
+        return self.model.predict(x)
+    
 
-        new_parameter, new_performance, data_scaler = eval_dataset.transform_data(parameter, performance)
+class Model500GELU(nn.Module):
+    def __init__(self, parameter_count=2, output_count=2):
+        super(Model500GELU, self).__init__()
+        self.network = nn.Sequential(
+            nn.Linear(parameter_count, 200),
+            nn.ReLU(),
+            nn.Linear(200, 300),
+            nn.ReLU(),
+            nn.Linear(300, 500),
+            nn.ReLU(),
+            nn.Linear(500, 500),
+            nn.ReLU(),
+            nn.Linear(500, 300),
+            nn.ReLU(),
+            nn.Linear(300, 200),
+            nn.ReLU(),
+            nn.Linear(200, output_count)
+        )
 
-        self.parameter = new_parameter
-        self.performance = new_performance
-        self.simulator = simulator
-        self.eval_dataset = eval_dataset
-        self.metric = metric
-        self.train_config = train_config
-        self.scaler = data_scaler
+    def forward(self, x):
+        return self.network(x)
+    
 
-        if train_config["model_type"] == 0:
-            self.model_wrapper = SklearnModelWrapper(model)
-        elif train_config["model_type"] == 1:
-            self.model_wrapper = PytorchModelWrapper(model, train_config, simulator)
-        else:
-            self.model_wrapper = LookupWrapper(self.simulator.sign)
+class Transformer(nn.Module):
 
-    def eval(self):
+    def __init__(
+        self,
+        parameter_count=2, 
+        output_count=2,
+        dim_model=200,
+        num_heads=2,
+        num_encoder_layers=6,
+        dim_hidden=200,
+        dropout_p=0.1,
+    ):
+        super(Transformer, self).__init__()
 
-        subset = self.train_config["subset"]
-        metrics_dict = generate_metrics_given_config(self.train_config)
-        
-        for index, percentage in enumerate(subset):
-            print("Running with percentage {}".format(percentage))
-            if percentage == 1 or percentage > 1:
-                raise ValueError("Subset Percentage must smaller than 1")
-            if np.gcd(int(percentage * 100), 100) + int(percentage * 100) != 100 \
-                    and np.gcd(int(percentage * 100), 100) != int(percentage * 100):
-                raise ValueError("Subset Percentage must be divisble")
+        self.embedding = nn.Linear(parameter_count, dim_model)
 
-            subset_metrics_dict = generate_metrics_given_config(self.train_config)
-            count = 0
-            for (parameter_train, parameter_test, performance_train, performance_test) in subset_split(self.parameter, self.performance, percentage, self.train_config["independent_sample"]):
-                count += 1
-                print("Run with {} percentage of training data, Run number {}".format(percentage, count))
-                kfold_metrics_dict = generate_metrics_given_config(self.train_config)
-                if self.train_config["subset_parameter_check"]:
-                    for subset_index in range(index + 1):
+        encoder_layers = TransformerEncoderLayer(dim_model, num_heads, dim_hidden, dropout_p, batch_first=True)
+        self.transformer = TransformerEncoder(encoder_layers, num_encoder_layers)
 
-                        subset_parameter_percentage = subset[subset_index]
-                        subset_parameter_size = int(parameter_train.shape[0] * (subset_parameter_percentage/percentage))
+        self.out = nn.Linear(dim_model, output_count)
 
-                        subsample_index = np.random.choice(np.arange(0, parameter_train.shape[0]),
-                                                           size=subset_parameter_size,
-                                                           replace=False)
-                        subsample_parameter = parameter_train[subsample_index,:]
-                        subsample_performance = performance_train[subsample_index, :]
-                        subsample_args = {
-                            "parameter": subsample_parameter,
-                            "performance": subsample_performance
 
-                        }
+    def forward(self, src, src_mask=None):
 
-                        new_train_parameter, new_train_performance,_ = self.eval_dataset.modify_data(parameter_train,
-                                                                                                     performance_train, parameter_test,
-                                                                                                     performance_test, train=True, extra_args=subsample_args)
-                        new_test_parameter, new_test_performance,_ = self.eval_dataset.modify_data(parameter_train, performance_train, parameter_test, performance_test, train=False)
+        src = self.embedding(src)
 
-                        result_eval_model = EvalModel(self.train_config, self.model_wrapper,
-                                                      new_train_parameter, new_train_performance,
-                                                      new_test_parameter, new_test_performance, self.simulator, self.scaler)
-                        subset_kfold_metrics_dict = result_eval_model.eval()
-                        merge_metrics(kfold_metrics_dict, subset_kfold_metrics_dict)
-                else:
-                    new_train_parameter, new_train_performance, _ = self.eval_dataset.modify_data(parameter_train,
-                                                                                                  performance_train,
-                                                                                                  parameter_test,
-                                                                                                  performance_test,
-                                                                                                  train=True)
-                    new_test_parameter, new_test_performance, _ = self.eval_dataset.modify_data(parameter_train,
-                                                                                                performance_train,
-                                                                                                parameter_test,
-                                                                                                performance_test,
-                                                                                                train=False)
+        if src_mask is None:
+            src_mask = nn.Transformer.generate_square_subsequent_mask(len(src))
 
-                    result_eval_model = EvalModel(self.train_config, self.model_wrapper,
-                                                  new_train_parameter, new_train_performance,
-                                                  new_test_parameter, new_test_performance, self.simulator, self.scaler)
-                    subset_kfold_metrics_dict = result_eval_model.eval()
-                    merge_metrics(kfold_metrics_dict, subset_kfold_metrics_dict)
-                merge_metrics(subset_metrics_dict, kfold_metrics_dict)
-            merge_metrics(metrics_dict, subset_metrics_dict)
-        return metrics_dict
+        transformer_out = self.transformer(src, src_mask)
+        out = self.out(transformer_out)
+
+        return out
