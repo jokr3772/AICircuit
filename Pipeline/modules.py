bd85a7d0a5a55811f65fba1c334854398785619c
Asal <asal.mehradfar@yahoo.com>
2024-05-17 14:50:39 -0700

    temp

---
+++ Pipeline/modules.py
@@ -0,0 +1,94 @@
+from Dataset.dataset import *
+import os
+
+from Utils.utils import load_circuit, saveDictToTxt
+from Model.models import *
+from Utils.visualutils import plot_multiple_loss_with_confidence_entrypoint
+
+
+def generate_dataset_given_config(circuit_config):
+
+    print("Return Dataset")
+    return BaseDataset(circuit_config["order"], circuit_config["sign"])
+
+
+def generate_circuit_given_config(circuit_name):
+
+    config_path = os.path.join(os.path.join(os.getcwd(), "Config"), "Circuits")
+    circuit_mapping = {
+        "singlestageamplifier": os.path.join(config_path, "SingleStageAmplifier.yaml"),
+        "cascode": os.path.join(config_path, "Cascode.yaml"),
+        "lna": os.path.join(config_path, "LNA.yaml"),
+        "mixer": os.path.join(config_path, "Mixer.yaml"),
+        "twostage": os.path.join(config_path, "TwoStage.yaml"),
+        "vco": os.path.join(config_path, "VCO.yaml"),
+        "pa": os.path.join(config_path, "PA.yaml"),
+        "vco_pa": os.path.join(config_path, "VCO_PA.yaml"),
+        "receiver": os.path.join(config_path, "Receiver.yaml"),
+        "transmitter-pa": os.path.join(config_path, "Transmitter-PA.yaml"),
+        "transmitter-vco": os.path.join(config_path, "Transmitter-VCO.yaml"),
+    }
+
+    if circuit_name.lower() in circuit_mapping:
+        circuit_definition_path = circuit_mapping[circuit_name.lower()]
+    else:
+        raise KeyError("The circuit you defined does not exist")
+
+    circuit = load_circuit(circuit_definition_path)
+    return circuit
+
+
+def generate_model_given_config(model_config,num_params,num_perf):
+
+    
+    sklearn_model_mapping = {
+        "RandomForestRegressor": RandomForest,
+        "SupportVectorRegressor": SupportVector,
+        "KNeighborsRegressor": KNeighbors,
+    }
+
+    dl_model_mapping = {
+        "MultiLayerPerceptron": Model500GELU,
+        "Transformer": Transformer
+    }
+
+    if model_config["model"] in sklearn_model_mapping.keys():
+        eval_model = sklearn_model_mapping[model_config["model"]]
+        copy_model_config = dict(model_config)
+        copy_model_config.pop("extra_args", None)
+        copy_model_config.pop("model", None)
+        return eval_model(**copy_model_config), 0
+    elif model_config["model"] in dl_model_mapping.keys():
+        model_config['parameter_count'] = num_perf
+        model_config['output_count'] = num_params
+        eval_model = dl_model_mapping[model_config["model"]]
+        copy_model_config = dict(model_config)
+        copy_model_config.pop("extra_args", None)
+        copy_model_config.pop("model", None)
+        return eval_model(**copy_model_config), 1
+    else:
+        raise KeyError("The model you defined does not exist")
+
+
+def generate_visual_given_result(result, train_config, visual_config, pipeline_save_name):
+    folder_path = os.path.join(os.path.join(os.getcwd(), "out_plot"), pipeline_save_name)
+    try:
+        os.mkdir(folder_path)
+    except:
+        pass #if less than a minute passed
+    result_dict = dict()
+
+    if train_config["loss_per_epoch"]:
+        loss_plot_result = plot_multiple_loss_with_confidence_entrypoint(train_config, visual_config, result, pipeline_save_name)
+        result_dict.update(loss_plot_result)
+    return result_dict
+
+
+def generate_circuit_status(parameter, performance, path):
+
+    circuit_dict = dict()
+    circuit_dict["num_parameter"] = parameter.shape[1]
+    circuit_dict["num_performance"] = performance.shape[1]
+    circuit_dict["data_size"] = performance.shape[0]
+
+    saveDictToTxt(circuit_dict, path)
