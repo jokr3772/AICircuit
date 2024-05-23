0b61cc4ea1ffbd7964a7e756d891a7fc694474ff
Asal <asal.mehradfar@yahoo.com>
2024-05-22 17:46:38 -0700

    minor change

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import argparse
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path', help='Train Config File Path', default="./config/train_config.yaml")
+    parser.add_argument('--path', help='Train Config File Path', default="./Config/train_config.yaml")
     args = parser.parse_args()
     config_path = args.path
 
