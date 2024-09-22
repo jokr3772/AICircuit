9b456235d03d04cea43dac4581359c585ab6cda6
Asal <asal.mehradfar@yahoo.com>
2024-09-22 16:54:41 -0700

    extra code commented

--- Simulation/simulator.py
+++ Simulation/simulator.py
@@ -44,47 +44,47 @@ class Simulator:
         if ret:
             print('[ERROR] cmd is not properly executed!!!')
     
-    def decode_results(self, sim_functions: list):
-        """Decode simulation results from output saved files
-        The simulation results are saved in a special format, we need first convert it to a text format,
-        and then extract the data we need
-        """
+    # def decode_results(self, sim_functions: list):
+    #     """Decode simulation results from output saved files
+    #     The simulation results are saved in a special format, we need first convert it to a text format,
+    #     and then extract the data we need
+    #     """
         
-        cur_sim_result = {}
+    #     cur_sim_result = {}
         
-        for func in sim_functions:
-            if 'ac' in func:
-                # convert results to text
-                cmd = f'{self.docker_cmd} \"cd {self.circuit_path_docker}; psf -i input_new.raw/ac.ac -o ac.txt\"'
-                ret = subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    #     for func in sim_functions:
+    #         if 'ac' in func:
+    #             # convert results to text
+    #             cmd = f'{self.docker_cmd} \"cd {self.circuit_path_docker}; psf -i input_new.raw/ac.ac -o ac.txt\"'
+    #             ret = subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
                 
-                if ret:
-                    print('[ERROR] cmd is not properly executed!!!')
+    #             if ret:
+    #                 print('[ERROR] cmd is not properly executed!!!')
                 
-                # extract data needed
-                text_ac_path = self.circuit_path + '/ac.txt'
-                freq, vout_mag = text.dec_ac_text(text_ac_path)
+    #             # extract data needed
+    #             text_ac_path = self.circuit_path + '/ac.txt'
+    #             freq, vout_mag = text.dec_ac_text(text_ac_path)
                 
-                # cur_sim_result['ac'] = [freq, vout_mag]
-                cur_sim_result['Bandwidth/Hz'] = bw_by_iterp(vout_mag, freq)
-                cur_sim_result['VoltageGain/dB'] = 20 * math.log10(vout_mag[0])
-                cur_sim_result['Error_Bandwidth/Hz'] = 0.0
-                cur_sim_result['Error_VoltageGain/dB'] = 0.0
+    #             # cur_sim_result['ac'] = [freq, vout_mag]
+    #             cur_sim_result['Bandwidth/Hz'] = bw_by_iterp(vout_mag, freq)
+    #             cur_sim_result['VoltageGain/dB'] = 20 * math.log10(vout_mag[0])
+    #             cur_sim_result['Error_Bandwidth/Hz'] = 0.0
+    #             cur_sim_result['Error_VoltageGain/dB'] = 0.0
                 
-            if 'dcOp' in func:
-                cmd = f'{self.docker_cmd} \"cd {self.circuit_path_docker}; psf -i input_new.raw/dcOp.dc -o dcOp.txt\"'
-                ret = subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    #         if 'dcOp' in func:
+    #             cmd = f'{self.docker_cmd} \"cd {self.circuit_path_docker}; psf -i input_new.raw/dcOp.dc -o dcOp.txt\"'
+    #             ret = subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
-                if ret:
-                    print('[ERROR] cmd is not properly executed!!!')
+    #             if ret:
+    #                 print('[ERROR] cmd is not properly executed!!!')
 
-                text_dc_path = self.circuit_path + '/dcOp.txt'
-                pw = text.dec_dc_text(text_dc_path)
+    #             text_dc_path = self.circuit_path + '/dcOp.txt'
+    #             pw = text.dec_dc_text(text_dc_path)
 
-                cur_sim_result['dcPowerConsumption/W'] = pw
-                cur_sim_result['Error_dcPowerConsumption/W'] = 0.0
+    #             cur_sim_result['dcPowerConsumption/W'] = pw
+    #             cur_sim_result['Error_dcPowerConsumption/W'] = 0.0
 
-        self.sim_results.append(cur_sim_result)
+    #     self.sim_results.append(cur_sim_result)
                 
     def get_results(self):
         """Simply extract result from results.txt generated from ocean
