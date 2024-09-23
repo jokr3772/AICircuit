246d4e6766c6cc2e17216957355dcf722b2389f9
Asal <asal.mehradfar@yahoo.com>
2024-09-23 12:13:27 -0700

    code cleaning

--- Simulation/simulator.py
+++ Simulation/simulator.py
@@ -3,10 +3,10 @@
 # Author: Yue (Julien) Niu
 
 import csv
-import math
 import subprocess
-from Simulation.utils.bw import bw_by_iterp
-from Simulation.utils import text
+
+
+DOCKER_CMD = 'docker exec --user=julien rlinux8-1 /bin/tcsh -c'
 
 
 class Simulator:
@@ -25,11 +25,12 @@ class Simulator:
         
         # construct simulation command with docker
         self.circuit_path_docker = circuit_path_docker
-        self.docker_cmd = 'docker exec --user=julien rlinux8-1 /bin/tcsh -c'
+        self.docker_cmd = DOCKER_CMD
         
         # store simulation results
         self.sim_results = []
-        
+
+
     def run_sim(self):
         """Start a simulation
         Note that all simulation parameters are defined in input.scs file.
@@ -43,48 +44,7 @@ class Simulator:
         
         if ret:
             print('[ERROR] cmd is not properly executed!!!')
-    
-    # def decode_results(self, sim_functions: list):
-    #     """Decode simulation results from output saved files
-    #     The simulation results are saved in a special format, we need first convert it to a text format,
-    #     and then extract the data we need
-    #     """
-        
-    #     cur_sim_result = {}
-        
-    #     for func in sim_functions:
-    #         if 'ac' in func:
-    #             # convert results to text
-    #             cmd = f'{self.docker_cmd} \"cd {self.circuit_path_docker}; psf -i input_new.raw/ac.ac -o ac.txt\"'
-    #             ret = subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-                
-    #             if ret:
-    #                 print('[ERROR] cmd is not properly executed!!!')
-                
-    #             # extract data needed
-    #             text_ac_path = self.circuit_path + '/ac.txt'
-    #             freq, vout_mag = text.dec_ac_text(text_ac_path)
-                
-    #             # cur_sim_result['ac'] = [freq, vout_mag]
-    #             cur_sim_result['Bandwidth/Hz'] = bw_by_iterp(vout_mag, freq)
-    #             cur_sim_result['VoltageGain/dB'] = 20 * math.log10(vout_mag[0])
-    #             cur_sim_result['Error_Bandwidth/Hz'] = 0.0
-    #             cur_sim_result['Error_VoltageGain/dB'] = 0.0
-                
-    #         if 'dcOp' in func:
-    #             cmd = f'{self.docker_cmd} \"cd {self.circuit_path_docker}; psf -i input_new.raw/dcOp.dc -o dcOp.txt\"'
-    #             ret = subprocess.call(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
-    #             if ret:
-    #                 print('[ERROR] cmd is not properly executed!!!')
-
-    #             text_dc_path = self.circuit_path + '/dcOp.txt'
-    #             pw = text.dec_dc_text(text_dc_path)
-
-    #             cur_sim_result['dcPowerConsumption/W'] = pw
-    #             cur_sim_result['Error_dcPowerConsumption/W'] = 0.0
-
-    #     self.sim_results.append(cur_sim_result)
                 
     def get_results(self):
         """Simply extract result from results.txt generated from ocean
@@ -104,6 +64,7 @@ class Simulator:
                     cur_sim_result['Error_'+metric] = 0
         self.sim_results.append(cur_sim_result)
 
+
     def run_all(self, n=10, display=True):
         """Run all simulations by sweeping paramters defined in the .csv file
         """
@@ -116,7 +77,7 @@ class Simulator:
                     self.circuit_params[p] = line[p]
             
                 # edit circuit parameters in .scs file
-                text.alter_circ_param(self.circuit_params, self.circuit_def)
+                alter_circ_param(self.circuit_params, self.circuit_def)
 
                 # start simulation
                 self.run_sim()
@@ -125,12 +86,14 @@ class Simulator:
                 self.get_results()
 
                 # calculate relative error
+                print(line)
                 self.calc_error(line)
 
-                if display and i > 10 and i % 100 == 0:
+                if display and i > 10 and i % 50 == 0:
                     print('{} points simulated.'.format(i+1))
 
                 if n != -1 and i == n - 1: break
+
 
     def calc_error(self, perf_ref):
         """Calculate error compared to reference values
@@ -153,3 +116,24 @@ class Simulator:
                     rel_error = abs(val_ref - val_actual) / abs(val_ref)
 
                 self.sim_results[-1]['Error_' + key] = rel_error
+
+
+def alter_circ_param(new_params_values, ocean_path):
+    scs_file = open(ocean_path, 'r')
+    lines = scs_file.readlines()
+    
+    # format for set variable values
+    format_var = 'desVar(   \"{}\" {} )\n'
+
+    # locate the line of circuit parameters
+    for i, line in enumerate(lines):
+        if 'desVar' in line:
+            var = line.split('\"')[1]
+            if var in new_params_values:
+                if new_params_values[var] == 0: continue
+                
+                lines[i] = format_var.format(var, new_params_values[var])
+
+    ocean_path_new = '/'.join(ocean_path.split('/')[0:-1]) + '/oceanScriptNew.ocn'
+    ocean_file_new = open(ocean_path_new, 'w')
+    ocean_file_new.writelines(lines)
